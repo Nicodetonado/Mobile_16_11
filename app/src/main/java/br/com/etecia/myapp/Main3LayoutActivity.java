81167f0e08d236736af99fea1d69036a36b42084
Nicodetonado <nicolass159753@hotmail.com>
2023-11-16 19:41:01 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/myapp/Main3LayoutActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main3LayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main3_layout);
+    }
+}
